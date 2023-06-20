a843eaaa553ef61903757758d8aff8bd11f7d224
pavankumar130 <20bcs130@iiitdmj.ac.in>
2023-06-20 12:08:10 +0530

    first commit

---
+++ src/Gallery.jsx
@@ -0,0 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import React from 'react'
+import { useGlobalContext } from './context'
+
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`
+
+const Gallery = () => {
+  const { searchTerm } = useGlobalContext()
+  const response = useQuery({
+    queryKey: ['images', searchTerm],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=${searchTerm}`)
+      return result.data
+    },
+  })
+  if (response.isLoading) {
+    return <section className="image-container">Loading.....</section>
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">There was an error.....</section>
+    )
+  }
+
+  const results = response.data.results
+  if (results.length < 1) {
+    return <section className="image-container">No results found.....</section>
+  }
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          ></img>
+        )
+      })}
+    </section>
+  )
+}
+
+export default Gallery
